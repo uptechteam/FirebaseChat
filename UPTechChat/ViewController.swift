a84da132ba385a8c75743a0b6cbcdfabd2fc4be4
Evgeny Matviyenko <evgeny.matviyenko@gmail.com>
2018-01-04 11:53:42 +0200

    Initial Commit

---
+++ UPTechChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UPTechChat
+//
+//  Created by Evgeny Matviyenko on 1/4/18.
+//  Copyright © 2018 upteachteam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
